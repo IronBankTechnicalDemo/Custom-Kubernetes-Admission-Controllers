6485da656630f5eb7e4392bb40af5a63382ec153
adam.toy <adam.toy@useitc.com>
2020-03-06 07:24:55 -0500

    Bug fixes

--- docker/application/Webhook.py
+++ docker/application/Webhook.py
@@ -2,18 +2,51 @@ from flask import Flask, request, jsonify
 import os, base64
 import jsonpatch
 import random
+import logging
 
 app = Flask(__name__)
 
 CERT = os.environ.get('CERT', 'certs/cert.pem')
 KEY = os.environ.get('KEY', 'certs/key.pem')
 
+class AdmissionResponse:
+    def __init__(self, a_type, allowed, message, patch=None):
+        self.a_type = a_type
+        self.allowed = allowed
+        self.message = message
+        self.patch = patch
+
+    def to_dict(self):
+        if self.a_type == 'validating':
+            return jsonify({
+                "response": {
+                    "allowed": self.allowed,
+                    "status": {
+                        "message": self.message
+                    }
+                }
+            })
+        elif self.a_type == 'mutating':
+            base64_patch = base64.b64encode(self.patch.to_string().encode("utf-8")).decode("utf-8")
+            return jsonify({
+                "response": {
+                    "allowed": self.allowed,
+                    "status": {
+                        "message": self.message
+                    },
+                    "patchType": "JSONPatch",
+                    "patch": base64_patch
+                }
+            })
+        else:
+            logging.error("ERROR: Invalid AdmissionType. Needs to be 'validating' or 'mutating'.")
+
 
 @app.route('/mutate/randomuid', methods=['POST'])
 def randomuid_webhook_mutate():
     """
     Ensures pods and deployments are ran as a Random UID with 'root' group ownership.
-    :return:
+    :return: Admission Mutation Response object.
     """
     request_info = request.get_json()
 
@@ -34,74 +67,67 @@ def randomuid_webhook_mutate():
     else:
         patches = []
 
-    return _admission_mutation_response(True, "Ensuring random UID", json_patch=jsonpatch.JsonPatch(patches))
+    return AdmissionResponse(
+        a_type='mutating',
+        allowed=True,
+        message = 'Ensuring random UID and Group',
+        patch=jsonpatch.JsonPatch(patches)
+    )
 
 
 @app.route('/validate/noprivilege', methods=['POST'])
 def block_privilege_webhook_validate():
     """
     Blocks deployment and pod creation if 'allowPrivilegeEscalation' is enabled.
-    :return: N/A
+    :return: Admission validation response object.
     """
     request_json = request.get_json()
 
     # Manages if the request is for a Deployment, since it has different pathing to get to Privilege Escalation.
     if request_json['request']['kind']['kind'] == 'Deployment':
         if 'securityContext' in request_json["request"]["object"]["spec"]['template']['spec'] and 'allowPrivilegeEscalation' in request_json["request"]["object"]["spec"]['template']['spec']['securityContext'] and request_json["request"]["object"]["spec"]['securityContext']['allowPrivilegeEscalation']:
-            return _admission_validation_response(False, "Privilege escalation not allowed.")
-        
-        for container_spec in request_json["request"]["object"]["spec"]["template']['spec']["containers"]:
+            logging.error('BLOCKED: Encountered pod attempting privilege escalation.')
+            return AdmissionResponse(
+                a_type='validating',
+                allowed=False,
+                message='Privilege escalation not allowed.'
+            )
+
+        for container_spec in request_json["request"]["object"]["spec"]['template']['spec']["containers"]:
+            logging.error('BLOCKED: Encountered container within attempting privilege escalation.')
             if 'securityContext' in container_spec and 'allowPrivilegeEscalation' in container_spec['securityContext'] and container_spec['securityContext']['allowPrivilegeEscalation']:
-                return _admission_validation_response(False, "Privilege escalation not allowed.")
-        
+                return AdmissionResponse(
+                    a_type='validating',
+                    allowed=False,
+                    message='Privilege escalation not allowed.'
+                )
+
     # Manages if the request is for a Pod, since it has different pathing to get to Privilege Escalation.                                                             
     elif request_json['request']['kind']['kind'] == 'Pod':
         if 'securityContext' in request_json["request"]["object"]["spec"] and 'allowPrivilegeEscalation' in request_json["request"]["object"]["spec"]['securityContext'] and request_json["request"]["object"]["spec"]['securityContext']['allowPrivilegeEscalation']:
-            return _admission_validation_response(False, "Privilege escalation not allowed.")
+            logging.error('BLOCKED: Encountered pod attempting privilege escalation.')
+            return AdmissionResponse(
+                a_type='validating',
+                allowed=False,
+                message='Privilege escalation not allowed.'
+            )
 
         for container_spec in request_json["request"]["object"]["spec"]["containers"]:
             if 'securityContext' in container_spec and 'allowPrivilegeEscalation' in container_spec['securityContext'] and container_spec['securityContext']['allowPrivilegeEscalation']:
-                return _admission_validation_response(False, "Privilege escalation not allowed.")
+                logging.critical('BLOCKED: Encountered container within pod attempting privilege escalation.')
+                return AdmissionResponse(
+                    a_type='validating',
+                    allowed=False,
+                    message='Privilege escalation not allowed.'
+                )
 
-    return _admission_validation_response(True, "Privilege escalation not detected. Approved.")
+    logging.info('ALLOWED: Deployment/Pod approved.')
+    return AdmissionResponse(
+        a_type='validating',
+        allowed=True,
+        message='Privilege escalation not detected. Approved.'
+    )
 
-
-def _admission_mutation_response(allowed, message, json_patch):
-    """
-    Get the valid response object for webhook.
-    :param allowed: Whether the admissino was allowed.
-    :param message: Message for logging.
-    :param json_patch: Patch to be appied.
-    :return:
-    """
-    base64_patch = base64.b64encode(json_patch.to_string().encode("utf-8")).decode("utf-8")
-    return jsonify({
-        "response": {
-            "allowed": allowed,
-             "status": {
-                 "message": message
-            },
-             "patchType": "JSONPatch",
-             "patch": base64_patch
-        }
-    })
-
-
-def _admission_validation_response(allowed, message):
-    """
-    Response for validating webhook.
-    :param allowed: Whether the action is allowed or not.
-    :param message: Status message
-    :return:
-    """
-    return jsonify({
-        "response": {
-            "allowed": allowed,
-            "status": {
-                "message": message
-            }
-        }
-    })
 
 def _get_random_uid():
     """
